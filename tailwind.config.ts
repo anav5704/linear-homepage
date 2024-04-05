3bd86cfea25351b5a6b24544ac45237f5b6428b1
anav5704 <anavkki@gmail.com>
2024-04-05 23:27:59 +1200

    build: initialise project

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: '"SF Pro Display",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu, Cantarell,"Open Sans","Helvetica Neue",sans-serif',
+            },
+            colors: {
+                transparent: "transparent",
+                white: "#fff",
+                "off-white": "#f7f8f8",
+                "transparent-white": "rgba(255, 255, 255, 0.08)",
+                background: "#000212",
+                grey: "#858699",
+                "grey-dark": "#222326",
+                "primary-text": "#b4bcd0",
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
+    },
+    plugins: [],
+}
+export default config
