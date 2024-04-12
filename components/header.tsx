198e1fc9dc12caf4076736e2c6f3b81320fc3363
anav5704 <anavkki@gmail.com>
2024-04-12 18:26:24 +1200

    feat: add button component

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,17 @@
 import { Container } from "./container"
 import { Logo } from '../public/logo'
 import Link from "next/link"
+import { Button } from "./button"
 
 export const Header = () => {
     return (
-        <header className="fixed top-0 left-0 w-full border-b border-white/10">
+        <header className="fixed top-0 left-0 w-full border-b border-white/10 backdrop-blur-[12px]">
             <Container className="w-3/4 text-[15px] font-medium h-[--nav-height]">
                 <nav className="h-full">
                     <ul className="flex items-center justify-between h-full">
                         <li>
                             <Link href="/" className="flex items-center" >
-                                <Logo className="h-5 w-5 mr-3" /> Linear
+                                <Logo className="h-[18px] w-[18px] mr-3" /> Linear
                             </Link>
                         </li>
                         <li>
@@ -38,7 +39,7 @@ export const Header = () => {
                             <Link href="/">Log In</Link>
                         </li>
                         <li>
-                            <button>Sign In</button>
+                            <Button href="/">Sign In</Button>
                         </li>
                     </ul>
                 </nav>
